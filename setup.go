895ed63707f7ca4b99b7dace4b5807f8e3b742c4
Zach Stone <zach@giantswarm.io>
2020-03-26 12:30:18 +0100

    Add load from file, update Readme

--- setup.go
+++ setup.go
@@ -1,7 +1,10 @@
 package example
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/coredns/coredns/core/dnsserver"
@@ -12,13 +15,23 @@ import (
 	"github.com/caddyserver/caddy"
 )
 
+type PluginOptions struct {
+	DomainFileName string
+	IPFileName     string
+}
+
 // init registers this plugin.
 func init() { plugin.Register("example", setup) }
 
 // setup is the function that gets called when the config parser see the token "example". Setup is responsible
 // for parsing any extra options the example plugin may have. The first token this function sees is "example".
 func setup(c *caddy.Controller) error {
-	c.Next() // Ignore "example" and give us the next token.
+
+	options, err := parseArguments(c)
+	if err != nil {
+		log.Error("Unable to parse arguments: ", err)
+	}
+
 	if c.NextArg() {
 		// If there was another token, return an error, because we don't have any configuration.
 		// Any errors returned from this setup function should be wrapped with plugin.Error, so we
@@ -27,7 +40,7 @@ func setup(c *caddy.Controller) error {
 	}
 
 	// Build the cache for the blacklist
-	blacklist, err := buildCache()
+	blacklist, err := buildCacheFromFile(options.DomainFileName)
 	if err != nil {
 		log.Error(err)
 	}
@@ -49,14 +62,62 @@ func setup(c *caddy.Controller) error {
 	return nil
 }
 
-func buildCache() (*mph.CHD, error) {
-	// TODO: Make this function take a config file and iterate through it
+func parseArguments(c *caddy.Controller) (PluginOptions, error) {
+	c.Next() // 0th token is the name of this plugin.
 
+	options := PluginOptions{}
+
+	i := 1
+	for c.Next() {
+		switch i {
+		case 1:
+			log.Info("Using domain blacklist file: ", c.Val())
+			options.DomainFileName = c.Val()
+		case 2:
+			log.Info("Using IP blacklist file: ", c.Val())
+			options.IPFileName = c.Val()
+		}
+		i++
+	}
+
+	if options.DomainFileName == "" {
+		log.Error("domain blacklist file is required")
+		return options, c.ArgErr()
+	}
+
+	return options, nil
+}
+
+func buildCacheFromFile(fileName string) (*mph.CHD, error) {
 	// Print a log message with the time it took to build the cache
 	defer logTime(time.Now(), "Building blacklist cache took %s")
 
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Error(err)
+	}
+	defer file.Close()
+
 	builder := mph.Builder()
-	builder.Add([]byte("example.org."), []byte(""))
+
+	// Known issue: the number of items in the cache must not be a power of 2
+	// Because... math. So no files with only 2 entries. Or 4. Or 8... etc.
+	// TODO: Get around this. Replace mph with custom hash set? Add a safe dummy blacklist item?
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		domain := scanner.Text()
+		// Assume all domains are global origin, with trailing dot (e.g. example.com.)
+		if !strings.HasSuffix(domain, ".") {
+			domain += "."
+		}
+		log.Info("Adding ", domain, " to domain blacklist")
+		builder.Add([]byte(domain), []byte(""))
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Error(err)
+	}
+
 	blacklist, err := builder.Build()
 
 	return blacklist, err
