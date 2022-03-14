d76ce4ae37bf899ae031aea9f2f6fcae19c7c4dc
Zach Stone <zach@giantswarm.io>
2022-03-14 16:43:14 +0100

    Update external plugin build

--- patch-plugin.sh
+++ patch-plugin.sh
@@ -8,6 +8,9 @@ plugincfg="plugin.cfg"
 # Add our plugin to the coredns plugin list
 sed -i '/^log:log/a warnlist:github.com/giantswarm/coredns-warnlist-plugin' "$target_base/$plugincfg"
 
+# Fetch the plugin from our repo
+go get github.com/giantswarm/coredns-warnlist-plugin
+
 # Add a replace to coredns's go.mod to use our local plugin
 if [ ! -z "$2" ]; then
     echo "Using local plugin path $2"
