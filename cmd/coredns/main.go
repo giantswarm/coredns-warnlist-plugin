5689eefdcc57fb9563a0a770339bedac58fe9f25
Zach Stone <zach@giantswarm.io>
2026-03-10 16:09:41 -0700

    Extend binary version with plugin version

--- cmd/coredns/main.go
+++ cmd/coredns/main.go
@@ -4,34 +4,42 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/coremain"
 
 	_ "github.com/coredns/coredns/core/plugin"
 
 	_ "github.com/giantswarm/coredns-warnlist-plugin"
+	"github.com/giantswarm/coredns-warnlist-plugin/pkg/project"
 )
 
-func main() {
-	var logIdx int = -1
-	for i, d := range dnsserver.Directives {
-		if d == "log" {
-			logIdx = i
-			break
+func init() {
+
+	var newDirectives []string
+	var addedWarnlist bool = false
+
+	for _, plugin := range dnsserver.Directives {
+		newDirectives = append(newDirectives, plugin)
+
+		if plugin == "log" {
+			if !addedWarnlist {
+				newDirectives = append(newDirectives, "warnlist")
+				addedWarnlist = true
+			}
 		}
 	}
 
-	if logIdx == -1 {
+	if !addedWarnlist {
 		fmt.Fprintln(os.Stderr, "Error: could not find 'log' plugin in CoreDNS directives")
 		os.Exit(1)
 	}
 
-	var newDirectives []string
-	newDirectives = append(newDirectives, dnsserver.Directives[:logIdx+1]...)
-	newDirectives = append(newDirectives, "warnlist")
-	newDirectives = append(newDirectives, dnsserver.Directives[logIdx+1:]...)
-
 	dnsserver.Directives = newDirectives
+}
 
+func main() {
+	// Add plugin version information to the CoreDNS version.
+	caddy.AppVersion = fmt.Sprintf("%s+warnlist-%s", coremain.CoreVersion, project.Version())
 	coremain.Run()
 }
